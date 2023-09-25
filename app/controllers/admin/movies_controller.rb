b637c97a1d3cf96a17c19512d2cf171e379f26ce
ISAWASHUN <syun20000616@icloud.com>
2023-09-25 15:52:59 +0900

    課題3終了

---
+++ app/controllers/admin/movies_controller.rb
@@ -0,0 +1,25 @@
+class Admin::MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to admin_movies_path, notice: '映画を登録しました'
+    else
+      flash.now[:alert] = 'エラーが発生しました'
+      render :new
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
+  end
+end
